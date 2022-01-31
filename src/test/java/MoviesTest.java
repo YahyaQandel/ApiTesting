c6ab655eb16175c9576948594631b002824ad06f
YahyaQandel <yahya.qandel@gmail.com>
2022-01-31 04:08:33 +0200

    refactor test cases class names

--- src/test/java/MoviesTest.java
+++ src/test/java/MoviesTest.java
@@ -10,14 +10,14 @@ import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class Movies {
+public class MoviesTest {
 
     private String API_URL = "https://movie-database-imdb-alternative.p.rapidapi.com/";
     private String API_HOST = "movie-database-imdb-alternative.p.rapidapi.com";
     private String API_KEY = "21e6f4f25dmsh8bbed3c079a43acp14bd7bjsnb34a3bcca5f8";
 
     @Test
-    public void getByName() {
+    public void assertThatSearchApiReturnsMatchingResults() {
         String movieName = "Avengers";
         Response response = given()
                 .contentType(ContentType.JSON)
